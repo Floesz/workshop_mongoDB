f1d2cba39da1a964c3e513058be9062c8569d54c
Guilherme Floes <guilhermefloesmmarchi@gmail.com>
2025-06-09 12:20:49 -0300

    consulta com @Query

--- src/main/java/com/guilherme/workshop/services/PostService.java
+++ src/main/java/com/guilherme/workshop/services/PostService.java
@@ -1,5 +1,7 @@
 package com.guilherme.workshop.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,5 +19,10 @@ public class PostService {
 	public Post findById(String id) {
 		return repository.findById(id)
 				.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado com ID: " + id));
+	}
+	
+	public List<Post> findByTitle(String text){
+		
+		return repository.searchTitle(text);
 	}
 }
