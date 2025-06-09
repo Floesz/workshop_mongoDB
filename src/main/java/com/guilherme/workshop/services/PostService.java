a035cd709f80a99014d16fa5c1d678c4b9f714aa
Guilherme Floes <guilhermefloesmmarchi@gmail.com>
2025-06-09 11:21:12 -0300

    método para buscar o post por id

---
+++ src/main/java/com/guilherme/workshop/services/PostService.java
@@ -0,0 +1,21 @@
+package com.guilherme.workshop.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.guilherme.workshop.domain.Post;
+import com.guilherme.workshop.repository.PostRepository;
+import com.guilherme.workshop.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repository;
+
+
+	public Post findById(String id) {
+		return repository.findById(id)
+				.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado com ID: " + id));
+	}
+}
